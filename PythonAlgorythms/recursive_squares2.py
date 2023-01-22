3bc617b14d0fa82f606dd95b6fec01a114f718a5
Vasya <vskesha@gmail.com>
2023-01-22 22:52:18 +0200

    changes

---
+++ PythonAlgorythms/recursive_squares2.py
@@ -0,0 +1,22 @@
+import graphics as gr
+
+size = 700
+alpha = 0.2
+window = gr.GraphWin("Recursive rectangles", size, size)
+
+
+def fractal_rectangle(A, B, C, D, deep=10):
+    if deep < 1:
+        return
+    for M, N in (A, B), (B, C), (C, D), (D, A):
+        gr.Line(gr.Point(*M), gr.Point(*N)).draw(window)
+    A1 = (A[0]*(1-alpha) + B[0]*alpha, A[1]*(1-alpha) + B[1]*alpha)
+    B1 = (B[0]*(1-alpha) + C[0]*alpha, B[1]*(1-alpha) + C[1]*alpha)
+    C1 = (C[0]*(1-alpha) + D[0]*alpha, C[1]*(1-alpha) + D[1]*alpha)
+    D1 = (D[0]*(1-alpha) + A[0]*alpha, D[1]*(1-alpha) + A[1]*alpha)
+    fractal_rectangle(A1, B1, C1, D1, deep-1)
+
+
+if __name__ == '__main__':
+    fractal_rectangle((100, 100), (500, 100), (500, 500), (100, 500))
+    print(window.getMouse())
---
+++ PythonAlgorythms/testing_some_moments.py
@@ -0,0 +1,14 @@
+def main():
+    mystr = "banana"
+    myit = iter(mystr)
+
+    print(next(myit))
+    print(next(myit))
+    print(next(myit))
+    print(next(myit))
+    print(next(myit))
+    print(next(myit))
+
+
+if __name__ == '__main__':
+    main()
