6abb6c4d235218ab928109d8fb0df032a5081872
Vasya <vskesha@gmail.com>
2023-02-25 23:42:48 +0200

    added 2 solutions to leetcode_solutions: longest_common_prefix.py roman_to_int.py

---
+++ leetcode_solutions/longest_common_prefix.py
@@ -0,0 +1,22 @@
+def longest_common_prefix(strs: list[str]) -> str:
+
+    def char_in_all_strings(i: int) -> bool:
+        char = strs[0][i]
+        for s in strs:
+            if s[i] != char:
+                return False
+        return True
+
+    min_len_str = min(strs, key=len)
+    min_len = len(min_len_str)
+    for i in range(min_len):
+        if not char_in_all_strings(i):
+            return strs[0][:i]
+
+    return min_len_str
+
+
+if __name__ == '__main__':
+    print(longest_common_prefix(["flower", "flow", "flight"]))
+    print(longest_common_prefix(["dog", "racecar", "car"]))
+    print(longest_common_prefix(['vsk', 'vske', 'vskesha']))
