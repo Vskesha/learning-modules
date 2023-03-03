15eee9644b44051feb089f6f5a033fe0947e816c
Vasya <vskesha@gmail.com>
2023-03-03 21:53:12 +0200

    exam_room.py changed

--- leetcode_solutions/exam_room.py
+++ leetcode_solutions/exam_room.py
@@ -1,49 +1,37 @@
+from bisect import insort
+
+
 class ExamRoom:
 
     def __init__(self, n: int):
-        self.state = [0] * n
+        self.state = []
         self.n = n
 
     def seat(self) -> int:
-        p = self.find_pos() if any(self.state) else 0
-        self.state[p] = 1
-        return p
+
+        if not self.state:
+            self.state.append(0)
+            return 0
+
+        pos = 0
+        prev = self.state[0]
+        max_dist = self.state[0]
+        for curr in self.state[1:]:
+            dist = (curr - prev) // 2
+            if max_dist < dist:
+                max_dist = dist
+                pos = prev + dist
+            prev = curr
+
+        if max_dist < self.n - prev - 1:
+            max_dist = self.n - prev - 1
+            pos = self.n -1
+
+        insort(self.state, pos)
+        return pos
 
     def leave(self, p: int) -> None:
-        self.state[p] = 0
-
-    def find_pos(self) -> int:
-
-        # 2222222222222222222222222222222222222222222222222222222222222222222222222
-        # pos = 0
-        # i = self.state.index(1)
-        # max_distance = i
-        # while True:
-        #     left = i
-        #     try:
-        #         d = self.state[i+1:].index(1) + 1
-        #         i += d
-        #         if max_distance < d // 2:
-        #             max_distance = d // 2
-        #             pos = left + max_distance
-        #     except ValueError:
-        #         d = self.n - left - 1
-        #         if max_distance < d:
-        #             pos = self.n - 1
-        #         break
-        # return pos
-        # 1111111111111111111111111111111111111111111111111111111111111111111111111111
-        # for i in range(self.n):
-        #     if self.state[i]:
-        #         if left == -1:  # There is noone on left. Take zero position
-        #             max_distance = i
-        #         elif max_distance < (i - left) // 2:  # place beetween left and right.
-        #             max_distance = (i - left) // 2
-        #             pos = left + max_distance
-        #         left = i
-        #     elif i == self.n - 1 and max_distance < self.n - left - 1:
-        #         pos = self.n - 1  # Place in the end
-        # return pos
+        self.state.remove(p)
 
 
 if __name__ == '__main__':
