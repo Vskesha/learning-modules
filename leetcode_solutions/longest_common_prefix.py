19fd9181ca6a25546eecc027115447cabfb96dd6
Vasya <vskesha@gmail.com>
2023-05-07 12:39:30 +0300

    changed 'longest_common_prefix.py' in 'leetcode_solutions' directory

--- leetcode_solutions/longest_common_prefix.py
+++ leetcode_solutions/longest_common_prefix.py
@@ -1,4 +1,14 @@
 def longest_common_prefix(strs: list[str]) -> str:
+    ans = 0
+    for letters in zip(*strs):
+        if len(set(letters)) == 1:
+            ans += 1
+        else:
+            break
+    return strs[0][:ans]
+
+
+def longest_common_prefix2(strs: list[str]) -> str:
 
     def char_in_all_strings(i: int) -> bool:
         char = strs[0][i]
@@ -20,3 +30,4 @@ if __name__ == '__main__':
     print(longest_common_prefix(["flower", "flow", "flight"]))
     print(longest_common_prefix(["dog", "racecar", "car"]))
     print(longest_common_prefix(['vsk', 'vske', 'vskesha']))
+    print(longest_common_prefix(["cir", "car"]))
