89b30e9af78d91ffe6d74a19f69ef482c8fa742c
Vasya <vskesha@gmail.com>
2023-09-04 12:56:43 +0300

    added 1 daily task to 'leetcode_solutions'

---
+++ leetcode_solutions/p141_linked_list_cycle.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
+
+
+class Solution1:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        while head:
+            if hasattr(head, 'pos'):
+                return True
+            head.pos = 0
+            head = head.next
+
+        return False
+
+
+class Solution2:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        while head:
+            if head in visited:
+                return True
+            visited.add(head)
+            head = head.next
+        return False
